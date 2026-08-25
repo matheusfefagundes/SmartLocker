1c38964037b6dfc036a7cceb526e8ea2c73be150
Matheus Ferreira Fagundes <matheusferreirafagundes23@gmail.com>
2026-08-25 20:22:42 -0300

    chore: troca da biblioteca bcrypt para bcryptjs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, Tamanho } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
